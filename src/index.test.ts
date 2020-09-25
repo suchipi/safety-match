8b7f5e769a33397c232a5b5bd70aab88f911981e
Lily Scott <me@suchipi.com>
2020-09-25 01:33:35 -0600

    Refactor TypeScript types to toplevel

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { makeTaggedUnion, MemberType, none } from "./index";
 
 describe("makeTaggedUnion", () => {
-  test("exhaustive matching", () => {
+  test("exhaustive matching behavior", () => {
     const foobar = makeTaggedUnion({
       FOO: none,
       BAR: none,
@@ -26,7 +26,37 @@ describe("makeTaggedUnion", () => {
     expect(ret).toBe(4);
   });
 
-  test("non-exhaustive matching", () => {
+  test("exhaustive matching types", () => {
+    const foobar = makeTaggedUnion({
+      FOO: none,
+      BAR: none,
+      BAZ: () => 6,
+    });
+
+    expect(foobar.FOO).toBeDefined();
+    expect(foobar.BAR).toBeDefined();
+
+    const value = foobar.FOO;
+    const ret = value.match({
+      FOO: () => 4,
+      BAR: () => 5,
+      BAZ: () => 6,
+    });
+    ret;
+
+    // hover over ret; it should be number
+
+    const ret2 = value.match({
+      FOO: () => 4,
+      BAR: () => "hi",
+      BAZ: () => undefined,
+    });
+    ret2;
+
+    // hover over ret2; it should be string | number | undefined
+  });
+
+  test("non-exhaustive matching behavior", () => {
     const foobar = makeTaggedUnion({
       FOO: none,
       BAR: none,
@@ -51,8 +81,50 @@ describe("makeTaggedUnion", () => {
     expect(ret).toBe(6);
   });
 
+  test("non-exhaustive matching types", () => {
+    const foobar = makeTaggedUnion({
+      FOO: none,
+      BAR: none,
+      BAZ: () => 6,
+    });
+
+    expect(foobar.FOO).toBeDefined();
+    expect(foobar.BAR).toBeDefined();
+
+    const value = foobar.BAZ();
+    const ret = value.match({
+      FOO: () => 5,
+      BAR: () => 6,
+      _: () => "hi",
+    });
+    ret;
+
+    // hover over ret; it should be string | number
+
+    const ret2 = value.match({
+      BAZ: (data) => data,
+      _: () => 0,
+    });
+    ret2;
+
+    // hover over ret2; it should be number
+
+    const ret3 = value.match({
+      FOO: () => 5,
+      BAR: () => 6,
+      _: (data) => data,
+    });
+    ret3;
+
+    // hover over ret3; ideally, it should be number, since the only remaining
+    // variant's data type is number. but it looks like it's number | undefined
+    // because the `data` going into the `_` case isn't refined to not include
+    // the data types for the FOO and BAR variants. Not ideal, but it's fine.
+  });
+
   test("with generics", () => {
-    // Generic support is not great, but it doesn't yell at least
+    // Generic support is not great, but it doesn't yell at least.
+    // the T below nevers gets a type, so it's `unknown`.
 
     const Maybe = makeTaggedUnion({
       Some: <T>(data: T) => ({ data }),
@@ -68,7 +140,8 @@ describe("makeTaggedUnion", () => {
   });
 
   test("with generics 2", () => {
-    // this one works better but it's not super discoverable
+    // this one works better, because T gets a type, but it's not super
+    // discoverable (and means you can't just have one Maybe type).
 
     function makeMaybe<T>() {
       return makeTaggedUnion({
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export const none = Symbol();
 
+export type None = typeof none;
+
 const MEMBER_TYPE = Symbol();
 
 export type MemberType<
@@ -8,94 +10,90 @@ export type MemberType<
   }
 > = TaggedUnionT[typeof MEMBER_TYPE];
 
-export function makeTaggedUnion<
-  T extends { [key: string]: typeof none | ((...args: any[]) => any) }
->(defObj: T) {
-  type DefObj = typeof defObj;
+type DefObjSuper = { [key: string]: None | ((...args: any[]) => any) };
 
-  type DataMap = {
-    [Property in keyof DefObj]: DefObj[Property] extends (...args: any) => any
-      ? ReturnType<DefObj[Property]>
-      : DefObj[Property] extends typeof none
-      ? undefined
-      : DefObj[Property];
-  };
+type DataMap<DefObj extends DefObjSuper> = {
+  [Property in keyof DefObj]: DefObj[Property] extends (...args: any) => any
+    ? ReturnType<DefObj[Property]>
+    : DefObj[Property] extends None
+    ? undefined
+    : DefObj[Property];
+};
 
-  type CasesObjFull = {
-    [Property in keyof DataMap]: DataMap[Property] extends typeof none
-      ? () => any
-      : (data: DataMap[Property]) => any;
-  };
+type CasesObjFull<DefObj extends DefObjSuper> = {
+  [Property in keyof DataMap<DefObj>]: DataMap<DefObj>[Property] extends None
+    ? () => any
+    : (data: DataMap<DefObj>[Property]) => any;
+};
 
-  type if_you_are_seeing_this_then_your_match_didnt_either_handle_all_cases_or_provide_a_default_handler_using_underscore = Partial<
-    CasesObjFull
-  > & {
-    _: <Property extends keyof DataMap>(data: DataMap[Property]) => any;
-  };
+type if_you_are_seeing_this_then_your_match_didnt_either_handle_all_cases_or_provide_a_default_handler_using_underscore<
+  DefObj extends DefObjSuper
+> = Partial<CasesObjFull<DefObj>> & {
+  _: <Property extends keyof DataMap<DefObj>>(
+    data: DataMap<DefObj>[Property]
+  ) => any;
+};
 
-  type MatchConfiguration =
-    | CasesObjFull
-    | if_you_are_seeing_this_then_your_match_didnt_either_handle_all_cases_or_provide_a_default_handler_using_underscore;
+type MatchConfiguration<DefObj extends DefObjSuper> =
+  | CasesObjFull<DefObj>
+  | if_you_are_seeing_this_then_your_match_didnt_either_handle_all_cases_or_provide_a_default_handler_using_underscore<
+      DefObj
+    >;
 
-  class MemberObject<Variant extends keyof DataMap> {
-    variant: Variant;
-    data: DataMap[Variant];
+class MemberObjectImpl {
+  variant: any;
+  data: any;
 
-    constructor(variant: Variant, data: DataMap[Variant]) {
-      this.variant = variant;
-      this.data = data;
-    }
-
-    match(casesObj: any): any {
-      const data = this.data;
-      const matchingHandler = casesObj[this.variant];
-
-      if (matchingHandler) {
-        return matchingHandler(data);
-      } else if (casesObj._) {
-        return casesObj._(data);
-      } else {
-        throw new Error(`Match did not handle variant: '${this.variant}'`);
-      }
-    }
+  constructor(variant: any, data: any) {
+    this.variant = variant;
+    this.data = data;
   }
 
-  // prettier-ignore
-  interface TaggedUnionMember
-  // @ts-ignore
-  <T extends {}>
-  {
-    match<C extends MatchConfiguration>(casesObj: C): ReturnType<
-      Exclude<C[keyof C], undefined>
-		>;
-		variant: keyof DefObj;
-		data: DataMap[keyof DefObj];
+  match(casesObj: any): any {
+    const data = this.data;
+    const matchingHandler = casesObj[this.variant];
+
+    if (matchingHandler) {
+      return matchingHandler(data);
+    } else if (casesObj._) {
+      return casesObj._(data);
+    } else {
+      throw new Error(`Match did not handle variant: '${this.variant}'`);
+    }
   }
+}
 
-  type TaggedUnion<T> = {
-    [Property in keyof T]: T[Property] extends (...args: any) => any
-      ? (...args: Parameters<T[Property]>) => TaggedUnionMember<T>
-      : TaggedUnionMember<T>;
-  } & {
-    [MEMBER_TYPE]: TaggedUnionMember<T>;
-  };
+interface MemberObject<DefObj extends DefObjSuper> {
+  match<C extends MatchConfiguration<DefObj>>(
+    casesObj: C
+  ): ReturnType<Exclude<C[keyof C], undefined>>;
+  variant: keyof DefObj;
+  data: DataMap<DefObj>[keyof DefObj];
+}
 
-  // @ts-ignore
-  const matchObj: TaggedUnion<T> = {};
+type TaggedUnion<DefObj extends DefObjSuper> = {
+  [Property in keyof DefObj]: DefObj[Property] extends (...args: any) => any
+    ? (...args: Parameters<DefObj[Property]>) => MemberObject<DefObj>
+    : MemberObject<DefObj>;
+} & {
+  [MEMBER_TYPE]: MemberObject<DefObj>;
+};
+
+export function makeTaggedUnion<DefObj extends DefObjSuper>(
+  defObj: DefObj
+): TaggedUnion<DefObj> {
+  const matchObj: any = {};
 
   Object.keys(defObj).forEach((matchType) => {
     const value = defObj[matchType];
 
     if (typeof value === "function") {
-      // @ts-ignore
       matchObj[matchType] = (...args: any) => {
         const data = value(...args);
-        // @ts-ignore
-        return new MemberObject(matchType, data);
+        return new MemberObjectImpl(matchType, data);
       };
     } else {
-      // @ts-ignore
-      matchObj[matchType] = new MemberObject(matchType);
+      matchObj[matchType] = new MemberObjectImpl(matchType, undefined);
     }
   });
 
